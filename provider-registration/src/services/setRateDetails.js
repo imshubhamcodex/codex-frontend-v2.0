b4073759fd94201fd911252ce9702b89f9960cdb
imshubhamcodex <edwssm777@gmail.com>
2021-06-20 21:43:06 +0530

    final commit v1

--- provider-registration/src/services/setRateDetails.js
+++ provider-registration/src/services/setRateDetails.js
@@ -9,9 +9,10 @@ async function setRates() {
 	for (let i = 0; i < state.services.length; i++) {
 		let service = state.services[i];
 		let obj = {
+			id: Math.Floor(Math.random() * 100 + 100),
 			organization_id: parseInt(service.orgID),
-			rate: parseInt(service.actualPrice),
-			service_id: parseFloat(service.id),
+			rate: parseFloat(service.actualPrice),
+			service_id: parseInt(service.id),
 			discounted_rate: parseFloat(service.discountedPrice),
 		};
 		await axios
